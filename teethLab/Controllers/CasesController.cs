8f6d5ef98120d7d526dff9bf0ff52a57e128d744
mostavaa <mos.hamed11@hotmail.com>
2017-01-07 11:45:13 +0200

    l filat elly msh mwgoda elly abo sala7 2al 3leha :D elly hya casescontroller w my files

---
+++ teethLab/Controllers/CasesController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace teethLab.Controllers
+{
+    public class CasesController : Controller
+    {
+        private teethLabEntities db = new teethLabEntities();
+
+        //
+        // GET: /Cases/
+
+        public ActionResult Index()
+        {
+            return View(db.CurrentCases.ToList());
+        }
+
+
+
+        //
+        // GET: /Cases/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Cases/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CurrentCas currentcas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CurrentCases.Add(currentcas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(currentcas);
+        }
+
+        //
+        // GET: /Cases/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            CurrentCas currentcas = db.CurrentCases.Find(id);
+            if (currentcas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(currentcas);
+        }
+
+        //
+        // POST: /Cases/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CurrentCas currentcas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(currentcas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(currentcas);
+        }
+
+        //
+        // GET: /Cases/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            CurrentCas currentcas = db.CurrentCases.Find(id);
+            if (currentcas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(currentcas);
+        }
+
+        //
+        // POST: /Cases/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CurrentCas currentcas = db.CurrentCases.Find(id);
+            db.CurrentCases.Remove(currentcas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ teethLab/Models/tools/myFile.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace teethLab
+{
+    public class myFile
+    {
+        public static string projectFolder = AppDomain.CurrentDomain.BaseDirectory;
+        public static void AppendStringToFile(string txt , string FilePath)
+        {
+            if (!File.Exists(projectFolder + FilePath))
+            {
+                File.Create(projectFolder+FilePath);
+            }
+            FileStream fs = new FileStream(projectFolder+FilePath, FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(txt);
+            sw.Close();
+            fs.Close();
+        }
+
+        public static void AppendLineToFile(string txt, string FilePath)
+        {
+            if (!File.Exists(projectFolder + FilePath))
+            {
+                File.Create(projectFolder+FilePath);
+            }
+            FileStream fs = new FileStream(projectFolder+FilePath, FileMode.Append );
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(txt);
+            sw.Close();
+            fs.Close();
+        }
+        
+        public static string[] readAllLines(string FilePath)
+        {
+            if (!File.Exists(projectFolder + FilePath))
+            {
+                File.Create(projectFolder+FilePath);
+            }
+            return File.ReadAllLines(projectFolder+FilePath);
+        }
+
+        public static void DeleteStringFromFile(string FilePath , string strToBeDeleted){
+            if (!File.Exists(projectFolder + FilePath))
+            {
+                File.Create(projectFolder+FilePath);
+                return;
+            }
+            string [] oldLines = myFile.readAllLines(projectFolder+FilePath);
+            List<string> newLines = new List<string>();
+            bool found = false;
+            foreach (var item in oldLines)
+            {
+                string newItem = item;
+                if (item.Contains(strToBeDeleted))
+                {
+                     newItem = item.Replace(strToBeDeleted, "");
+                     found = true;
+                }
+                newLines.Add(newItem);
+            }
+            if (found) 
+            File.WriteAllLines(projectFolder + FilePath, newLines);
+        }
+
+        public static void DeleteLineFromFile(string FilePath, string LineToBeDeleted)
+        {
+            if (!File.Exists(projectFolder + FilePath))
+            {
+                File.Create(projectFolder+FilePath);
+                return;
+            }
+            
+            string[] oldLines = myFile.readAllLines(projectFolder + FilePath);
+            List<string> newLines = new List<string>();
+            bool found = false;
+            foreach (var item in oldLines)
+            {
+                
+                if (item.ToLower() == (LineToBeDeleted.ToLower()))
+                {
+                    found = true;
+                    continue;
+                }
+                newLines.Add(item);
+            }
+            if (found)
+                File.WriteAllLines(projectFolder + FilePath, newLines);
+        }
+
+    }
+}
